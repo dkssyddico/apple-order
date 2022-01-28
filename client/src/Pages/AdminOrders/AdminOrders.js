20f35e6244a5467f63fb8baf2a16afa84879c659
dkssyddico <dkssyddico@naver.com>
2022-01-28 22:24:46 +0900

    style: admin orders page

--- client/src/Pages/AdminOrders/AdminOrders.js
+++ client/src/Pages/AdminOrders/AdminOrders.js
@@ -1,58 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import orderService from '../../service/order';
+import styles from './AdminOrders.module.css';
 
 function AdminOrders() {
-  const [orders, setOrders] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const { isLoading, isError, data, error } = useQuery('adminOrders', async () => {
+    let { data } = await orderService.getAllOrders();
+    return data;
+  });
 
-  const getOrders = async () => {
-    try {
-      let {
-        data: { orders },
-      } = await orderService.getAllOrders();
-      setOrders(orders);
-    } catch (error) {
-      let {
-        response: {
-          data: { message },
-        },
-      } = error;
-      setError(true);
-      setErrorMessage(message ? message : error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getOrders();
-  }, []);
+  if (isLoading) {
+    return (
+      <div className={styles.adminOrders}>
+        <p>Now Loading...</p>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className={styles.adminOrders}>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
 
   return (
-    <div className='container'>
-      <h1>Admin Orders </h1>
-      {error && <h1>{errorMessage}</h1>}
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {orders.map((order) => (
-            <div key={order._id}>
-              <div>
-                <Link to={`/admin/orders/${order._id}`}>
-                  <p>{order._id}</p>
-                </Link>
-              </div>
-              <div>
-                <p>{order.createdAt}</p>
-              </div>
+    <div className={styles.adminOrders}>
+      <h1 className={styles.title}>Admin Orders</h1>
+      <div className={styles.headBox}>
+        <p className={styles.head}>Order Id.</p>
+        <p className={styles.head}>Date</p>
+      </div>
+      <div className={styles.contentContainer}>
+        {data.orders &&
+          data.orders.map((order) => (
+            <div className={styles.contentBox} key={order._id}>
+              <p className={styles.content}>
+                <Link to={`/admin/orders/${order._id}`}>{order._id} </Link>
+              </p>
+              <p className={styles.content}>{`${order.createdAt.split('T')[0]} ${
+                order.createdAt.split('T')[1].split('.')[0]
+              }`}</p>
             </div>
           ))}
-        </div>
-      )}
+      </div>
     </div>
   );
 }
