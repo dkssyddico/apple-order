6daa03b7fa85a98bd8892e9270ce5adf95692f54
dkssyddico <dkssyddico@naver.com>
2022-01-30 23:43:13 +0900

    style: order success page

--- client/src/Pages/OrderSuccess/OrderSuccess.js
+++ client/src/Pages/OrderSuccess/OrderSuccess.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import styles from './OrderSuccess.module.css';
 
 function OrderSuccess() {
   const {
@@ -7,18 +8,23 @@ function OrderSuccess() {
   } = useLocation();
 
   return (
-    <div className='container'>
-      <div>
-        <h1>주문이 완료되었습니다!</h1>
-        <p>주문번호 {orderId}</p>
-        <div>
-          <button>
-            <Link to='/'>메인으로 돌아가기</Link>
-          </button>
-          <button>
-            <Link to={`/orders/${orderId}`}>주문 상세내역 보기</Link>
-          </button>
-        </div>
+    <div className={styles.orderSuccess}>
+      <div className={styles.infoContainer}>
+        <h1>Thank you for your order!</h1>
+        <p>
+          Order number is{' '}
+          <Link to={`/orders/${orderId}`}>
+            <span className={styles.orderId}>{orderId}</span>
+          </Link>{' '}
+        </p>
+      </div>
+      <div className={styles.btnContainer}>
+        <button className={styles.mainBtn}>
+          <Link to='/'>Go to Main</Link>
+        </button>
+        <button className={styles.detailBtn}>
+          <Link to={`/orders/${orderId}`}>Go to Order Detail</Link>
+        </button>
       </div>
     </div>
   );
