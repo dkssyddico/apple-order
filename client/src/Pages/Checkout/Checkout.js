f533f8dab34a89ce3eef8dfe9dd9e44c559232e6
dkssyddico <dkssyddico@naver.com>
2022-01-30 23:14:05 +0900

    style: checkout page

--- client/src/Pages/Checkout/Checkout.js
+++ client/src/Pages/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { refreshCart } from '../../reducers/cartReducer';
 import orderService from '../../service/order';
+import styles from './Checkout.module.css';
 
 function Checkout() {
   const {
@@ -46,45 +47,53 @@ function Checkout() {
   }, [items, navigate]);
 
   return (
-    <div className='container'>
-      <h1>Checkout</h1>
-      {error && <h1>{errorMessage}</h1>}
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Product image</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items &&
-              items.map((item) => {
-                return (
-                  <tr key={uuidv4()}>
-                    <td>
-                      <img
-                        style={{ width: '70px' }}
-                        src={`http://localhost:4000/${item.images[0].filePath}`}
-                        alt='product'
-                      />
-                    </td>
-                    <td>{item.name}</td>
-                    <td>{item.canBeSold ? item.price : '0'}</td>
-                    <td>{item.quantity}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
-      <div>
-        <h1>Total price: {items.reduce((a, b) => b.canBeSold && a + b.price * b.quantity, 0)}</h1>
-        <h1>Total items: {items ? items.filter((item) => item.canBeSold).length : 0}</h1>
-        <button onClick={() => handlePaymentClick()}>Payment</button>
-      </div>
+    <div className={styles.checkout}>
+      <h1 className={styles.title}>checkout</h1>
+      {error && <p>{errorMessage}</p>}
+      <section className={styles.checkoutContainer}>
+        <div className={styles.headContainer}>
+          <div className={styles.head}>
+            <h2>Product</h2>
+          </div>
+          <div className={styles.head}>
+            <h2>Price</h2>
+          </div>
+          <div className={styles.head}>
+            <h2>Quantity</h2>
+          </div>
+        </div>
+        <div className={styles.contentContainer}>
+          {items.map((item) => (
+            <div className={styles.contents}>
+              <section className={styles.content}>
+                <div>
+                  <img
+                    className={styles.image}
+                    src={`http://localhost:4000/${item.images[0].filePath}`}
+                    alt='product'
+                  />
+                </div>
+                <p className={styles.itemName}>{item.name}</p>
+              </section>
+              <section className={styles.content}>
+                <span>${item.price * item.quantity}</span>
+              </section>
+              <section className={styles.content}>
+                <span>{item.quantity}</span>
+              </section>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={styles.summaryContainer}>
+        <p>Total price: {items.reduce((a, b) => b.canBeSold && a + b.price * b.quantity, 0)}</p>
+        <p>Total items: {items ? items.filter((item) => item.canBeSold).length : 0}</p>
+      </section>
+      <section className={styles.btnContainer}>
+        <button className={styles.paymentBtn} onClick={() => handlePaymentClick()}>
+          Payment
+        </button>
+      </section>
     </div>
   );
 }
